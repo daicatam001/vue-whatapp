f25cf58dcb17c767ba91292844496406f65110ca
tampt <tampt@gmail.com>
2021-08-15 21:43:50 +0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,24 @@
 module.exports = {
-  rules: {
-    "no-mixed-spaces-and-tabs":0
-  }
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        "plugin:vue/base",
+        "plugin:vue/vue3-essential",
+        "plugin:vue/vue3-strongly-recommended",
+        "plugin:vue/vue3-recommended",
+        // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+        "prettier"
+    ],
+    rules: {
+        "no-mixed-spaces-and-tabs": 0,
+        "vue/require-prop-types": 0,
+        "vue/script-indent": ["error", 2, {"baseIndent": 0}],
+        "vue/html-indent": ["error", 2, {
+            "attribute": 2,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }]
+    }
 };
--- src/components/chat-list/ChatList.vue
+++ src/components/chat-list/ChatList.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="chat-app">
-	{{chats}}
+  <div class="chat-list">
+    <div class="chat-cart-list">
+      <ChatCard 
+      :key="chat.id"
+      v-for="chat of chats" 
+      :title="chat.title" />
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        computed: {
-            chats() {
-                return this.$store.getters['chats/chats']
-            }
-        },
-        created() {
-            this.$store.dispatch('chats/getLatestChats', 25)
-        }
-    }
+import  ChatCard from './ChatCard'
+  {
+     components:{
+         ChatCard
+     },
+     computed: {
+         chats() {
+             return this.$store.getters['chats/chats']
+         }
+     },
+     created() {
+         this.$store.dispatch('chats/getLatestChats', 25)
+     }
+ }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

20a0ce9f760158399a78cb9c731ff5d077c8df55
tampt <tampt95@gmail.com>
2021-09-26 22:57:30 +0700

    fea: evn variable

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,8 +1,8 @@
-export const PROJECT_ID = '07be9561-163e-42a3-9ba9-4e056837fe04'
+export const PROJECT_ID =  process.env.VUE_APP_CHAT_ENGINE_PROJECT_ID || '07be9561-163e-42a3-9ba9-4e056837fe04'
 
-export const PRIVATE_KEY = '0b611334-6a16-406e-82d6-cd2cd59a1655'
+export const PRIVATE_KEY = process.env.VUE_APP_CHAT_ENGIN_PRIVATE_KEY || '0b611334-6a16-406e-82d6-cd2cd59a1655'
 
-export const SERVER_URL = 'https://api.chatengine.io'
+export const SERVER_URL = process.env.VUE_APP_SERVER_URL || 'https://api.chatengine.io'
 
 export enum LOAD_STATE {
   UNLOAD,
